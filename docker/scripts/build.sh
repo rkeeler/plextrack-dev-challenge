e06bc9890ce813b9faa11a5abf98528b993eb4a2
Reagan Keeler <6854684+rkeeler@users.noreply.github.com>
2021-02-25 12:12:25 -0700

    copy Dockerfile into build folder

--- docker/scripts/build.sh
+++ docker/scripts/build.sh
@@ -5,3 +5,6 @@ cp -r server/build docker/build
 # build frontend and copy into server's public folder
 yarn --cwd frontend build
 cp -r frontend/build docker/build/public
+
+# copy Dockerfile into build folder
+cp -r docker/Dockerfile docker/build/Dockerfile
